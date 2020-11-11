eeddcb9e55358a028378d3343669a6930e335f48
Daniel <admg@ciencias.unam.mx>
2020-11-11 14:29:06 -0600

    Modified Allolactase parameter according to manual, added h

--- Lactose_Project/Lactose_project.R
+++ Lactose_Project/Lactose_project.R
@@ -6,14 +6,18 @@ source("grind.R")
 model <- function(t, state, parms) {
   with(as.list(c(state,parms)), {
     R = 1/(1+A^n) # Repressor
-    dA = M*L - delta*A - v*M*A # Allolactose
+    ###Modificando Allolactasa de acuerdo al manual
+    ###Se comentara la formula original
+    #dA = M*L - delta*A - v*M*A # Allolactose OLD
+    ##Se agrega la division entre h+A
+    dA = M*L - delta*A - ((v*M*A) / (h+A))
     dM = c0 + c*(1-R) - d*M # mRNA
     return(list(c(dA, dM)))
   })
 }
 
-###Definiendo Parametros
-p <- c(L=1,c=1,c0=0.05,d=1,delta=0.2,n=5,v=0.25)
+###Definiendo Parametros por default
+p <- c(L=1,c=1,c0=0.05,d=1,delta=0.2,n=5,v=1, h = 2)
 s <- c(A=0,M=0)
 plane(xmax=4)
 low <- newton(s,plot=T)
